6d22f0c2dceae42bcdad9fbbbdf8225576c68080
LoganEndreman <pengellylogan@gmail.com>
2024-01-29 17:50:37 -0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,19 +1,24 @@
-// swift-tools-version: 5.9
+// swift-tools-version:5.3
 import PackageDescription
 
 let package = Package(
     name: "Login-Experiment",
     products: [
-        .executable(name: "Login-Experiment", targets: ["Login-Experiment"])
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .executable(
+            name: "Login-Experiment",
+            targets: ["Login-Experiment"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/CodeSlicing/pure-swift-ui.git", .exact("2.1.1")),
-        .package(url: "https://github.com/CodeSlicing/pure-swift-ui-tools.git", .exact("2.0.2"))
+        // Dependencies declare other packages that this package depends on.
+        .package(name: "PureSwiftUI", url: "https://github.com/CodeSlicing/pure-swift-ui.git", .upToNextMajor(from: "2.1.1")),
+        .package(name: "PureSwiftUITools", url: "https://github.com/CodeSlicing/pure-swift-ui-tools.git", .upToNextMajor(from: "2.0.2")),
     ],
     targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "Login-Experiment",
-            dependencies: ["PureSwiftUI", "PureSwiftUITools"]
-        ),
+            dependencies: ["PureSwiftUI", "PureSwiftUITools"]),
     ]
 )
