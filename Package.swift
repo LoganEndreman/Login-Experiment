34cc57e983bbed45377597af04c4b5cbf392175a
LoganEndreman <pengellylogan@gmail.com>
2025-01-06 21:28:02 -0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -19,6 +19,6 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "Login-Experiment",
-            dependencies: ["pure-swift-ui", "pure-swift-ui-tools"]),
+            dependencies: ["PureSwiftUI", "PureSwiftUITools"]),
     ]
 )
